0eeaeacdb55774381237add82365043ee8960f70
manami-m7 <kmuuum@gmail.com>
2020-11-30 06:51:35 +0000

    [Update4-10]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_102401) do
+ActiveRecord::Schema.define(version: 2020_11_30_064437) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
