69b6e64201f21f08df20cf79c4c8c97b2adc418d
manami-m7 <kmuuum@gmail.com>
2020-12-01 08:56:44 +0000

    [Update4-10]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,9 +19,14 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,18 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @book = Book.new
   end
 
-  # def new
-  #   @book = Book.new
-  # end
+  def new
+    @book = Book.new
+  end
 
-  # def create
-  #   @book = Book.new(book_params)
-  #   @book.user_id = current_user.id
-  #   @book.save
-  #   redirect_to book_path(@book)
-  # end
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
 
 end
