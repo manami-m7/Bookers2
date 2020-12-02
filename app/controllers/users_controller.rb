608993499eb85d8fb3d228b3eec23341abba5118
manami-m7 <kmuuum@gmail.com>
2020-12-02 13:26:51 +0000

    [Update4-10]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,12 @@ class UsersController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+      flash[:notice] = "Welcome! You have signed up successfully."
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
   end
 
   def index
@@ -25,7 +29,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    user_id == current_user.id
+    @user = current_user
     if @user.update(user_params)
       flash[:notice] = "You have updated user succeessfully."
       redirect_to user_path(@user)
@@ -34,4 +38,9 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
 
-  resources :homes, only: [:top]
+  resources :homes, only: [:top, :about]
   resources :books
   resources :users
 end
