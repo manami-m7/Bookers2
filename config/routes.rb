cbbfea3ef7f798fce95eea676aaf96beee14a7ef
manami-m7 <kmuuum@gmail.com>
2020-12-20 07:37:47 +0000

    [応用課題５]

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
+  get 'search/search'
   devise_for :users
   root to: 'homes#top'
-  get "home/about" => "homes#about", as: "about"
+  get 'home/about' => 'homes#about', as: 'about'
+  get '/search' => 'search#search'
   resources :homes
   resources :books do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-
   resources :users do
     member do
       get :followed, :followers
     end
   end
-
   resources :relationships, :only => [:create, :destroy]
+  
+  
 
 
 end
