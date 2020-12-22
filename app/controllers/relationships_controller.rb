6d3e0cb87e79074a5bf5302f0d146fbf8eebad3c
manami-m7 <kmuuum@gmail.com>
2020-12-22 07:19:39 +0000

    [応用課題６]

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -7,7 +7,7 @@ class RelationshipsController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
+    @user = User.find(params[:followed_id])
     current_user.unfollow(@user)
     redirect_back(fallback_location: root_path)
   end
